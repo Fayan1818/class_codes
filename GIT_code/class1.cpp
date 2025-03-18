847a3a111eaef6c5975cc1e0e9d9ec0cd8e2b6c1
Fayan1818 <24ce122@charusat.edu.in>
2025-03-18 08:01:59 +0530

    Add files via upload

---
+++ GIT_code/class1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main(){
+
+    
+cout<<"StudentID"<<setw(4)<<":"<<setw(1)<<"24CE122"<<endl;
+    cout<<"Student Name"<<setw(2)<<":"<<setw(1)<<"Fayan Suthar"<<endl<<endl;
+    cout<<setw(25)<<"TH"<<setw(3)<<"PR"<<endl;
+    cout<<"Computer Programming"<<setw(5)<<"A+"<<setw(3)<<"B "<<endl;
+    cout<<"Math"<<setw(21)<<"B+"<<setw(3)<<"O+"<<endl;
+    cout<<endl<<endl<<"SGPA"<<setw(1)<<":"<<setw(1)<<"6.84"<<endl;
+    cout<<"CGPA"<<setw(1)<<":"<<setw(1)<<"6.84"<<endl;
+}
